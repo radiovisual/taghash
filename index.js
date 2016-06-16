d424b8a1d7132b7fa3461f4611336b7f63111814
Michael Wuergler <wuergler@gmail.com>
2016-06-16 15:30:26 +0100

    1.2.0

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 'use strict';
 
 module.exports = function (str, options) {
-    var href = 'https://twitter.com/hashtag/';
-
-    if (options && options.href) {
-        href = options.href;
+    if (typeof str !== 'string') {
+        throw new TypeError('taghash expects a string');
     }
 
-    if (!(/#/).test(str)) {
+    options = options || {};
+    options.href = options.href || 'https://twitter.com/hashtag/';
+
+    if (!(/#/g).test(str)) {
         return str;
     }
 
     var hashes = str.match(/[^|\s]?#[\d\w]+/g);
+
+    if (!hashes) {
+        return str;
+    }
 
     var s = str;
 
@@ -20,8 +25,9 @@ module.exports = function (str, options) {
 
         // ignore tags that have a double "##"
         if (hash.substring(0, 1) !== '#') {
-            s = s.replace(item, '<a href="' + href + '' + hash + '">' + item + '</a>');
+            s = s.replace(item, '<a href="' + options.href + '' + hash + '">' + item + '</a>');
         }
     });
+
     return s;
 };
--- test.js
+++ test.js
@@ -4,59 +4,46 @@ var taghash = require('./');
 
 describe("taghash", function(){
 
+    it('expects a string', function(){
+        assert.throws(function() {
+            taghash();
+        }, 'taghash expects a string');
+    });
+
     it('should wrap plain-text hashtags with twitter-ready anchor tags', function(){
-
         assert.equal(taghash("#hashtag"), "<a href=\"https://twitter.com/hashtag/hashtag\">#hashtag</a>");
-
     });
 
     it('should convert multiple hashtags in string', function(){
-
         assert.equal(taghash("Shoutout! #hashtag01 #hashtag02"), "Shoutout! <a href=\"https://twitter.com/hashtag/hashtag01\">#hashtag01</a> <a href=\"https://twitter.com/hashtag/hashtag02\">#hashtag02</a>");
-
     });
 
     it('should return unmodified string if string has no hashtags', function(){
-
         assert.equal(taghash("Shoutout!"), "Shoutout!");
-
     });
 
     it('should ignore any hashtag values preceded by `##`', function(){
-
         assert.equal(taghash("##Shoutout!"), "##Shoutout!");
-
     });
 
     it('should still work on valid values, and skip invalid values', function(){
-
         assert.equal(taghash("##Shoutout! #yo"), "##Shoutout! <a href=\"https://twitter.com/hashtag/yo\">#yo</a>");
-
     });
 
     it('should find hastags at the beginning of a string', function(){
-
         assert.equal(taghash("#yo! howdy peeps"), "<a href=\"https://twitter.com/hashtag/yo\">#yo</a>! howdy peeps");
-
     });
 
     it('should find hastags at the end of a string', function(){
-
         assert.equal(taghash("howdy peeps #yo!"), "howdy peeps <a href=\"https://twitter.com/hashtag/yo\">#yo</a>!");
-
     });
 
     it('should allow a custom src passed in', function(){
         assert.equal(taghash("howdy peeps #yo!", {href: "http://example.com/"}), "howdy peeps <a href=\"http://example.com/yo\">#yo</a>!")
     });
 
-
-
-
-
-
-
-
-
-
+    it('should know how to recover from text it can\'t read', function() {
+        var text = '#رمضان مبارک: وزير #اشتاینمایر #افطار می کند با همکار خود @Jzarif از #ایران https://t.co/MSurTL0Mvn';
+        assert.equal(taghash(text), text);
+    });
 });
